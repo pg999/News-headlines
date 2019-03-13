662dc698c2c2731ac5701d3798bb12b740140b9a
Pramud Gupta <pramudgupta@Pramuds-MacBook-Air.local>
2019-03-14 02:27:22 +0530

    BAckup 2:27am

--- search.py
+++ search.py
@@ -1,35 +1,65 @@
 from textblob import TextBlob
 from bs4 import BeautifulSoup
+import pandas as pd
 import requests
-
 def headlines_timesnow(keyword):
-	text_file = open("Output_TimesNow.txt", "w")
+	dataframe_obj = 0
+	data = {'News Portal':[], 'Headlines': [],'URL':[],'Date':[]}
 	for i in range(0,10):
 		url = 'https://www.timesnownews.com/searchresults/0/'+ str(i) +'?searchterm={0}'.format(keyword)
 		response = requests.get(url)
 		soup = BeautifulSoup(response.text, 'html.parser')
 		headline_results = soup.find_all('a', class_='component_2')
-
-		for txt in headline_results:
-			headline = txt.find('div',class_="text")
-			date  = txt.find('div',class_ = "datetimes")
-			text_file.write("HEADLINE: "+ headline.text+"\nDATE:"+ date.text+"\n\n")
-	text_file.close()
+		for item in headline_results:
+			headline = item.find('div',class_="text")
+			date  = item.find('div',class_ = "datetimes")
+			data['News Portal'].append("Times Now")
+			data['Headlines'].append(headline.text)
+			data['URL'].append(str(item['href']))
+			data['Date'].append(date.text)
+		dataframe_obj = pd.DataFrame(data,columns=('News Portal','Headlines','URL','Date'))
+	return dataframe_obj
 
 def headlines_thebetter_india(keyword):
-	text_file = open("Output_TheBetterIndia.txt", "w")
-	for i in range(1,2):
+	dataframe_obj=0
+	data = {'News Portal':[], 'Headlines': [],'URL':[],'Date':[]}
+	for i in range(1,11):
 		url = 'https://www.thebetterindia.com/page/'+str(i)+'/?s={0}'.format(keyword)
 		response = requests.get(url)
 		soup = BeautifulSoup(response.text, 'html.parser')
 		headline_results = soup.find_all('li', class_="g1-collection-item")
-		for txt in headline_results:
-			headline = txt.find('h3')
-			text_file.write("HEADLINE: "+ headline.find('a').text+"\nDATE:"+ str(txt.find('time')['datetime'])+"\nURL:"+str(headline.find('a')['href'])+"\n\n")
-	text_file.close()
+		for item in headline_results:
+			headline = item.find('h3')
+			data['News Portal'].append("The Better India")
+			data['Headlines'].append(headline.find('a').text)
+			data['URL'].append(str(headline.find('a')['href']))
+			data['Date'].append(str(item.find('time').text))
+		dataframe_obj = pd.DataFrame(data,columns=('News Portal','Headlines','URL','Date'))
+	return dataframe_obj
 
+def headlines_indiatoday(keyword):
+	dataframe_obj = 0
+	data = {'News Portal':[], 'Headlines': [],'URL':[],'Date':[]}
+	for i in range(0,10):
+		url = 'https://www.indiatoday.in/topic/{0}?page='.format(keyword)+str(i)
+		response = requests.get(url)
+		soup = BeautifulSoup(response.text, 'html.parser')
+		headline_results = soup.find_all('li', {"class":"itg-search-list"})
+		for item in headline_results:
+			headline = item.find('div',class_="front-label")
+			link = headline.find('a')['href']
+			date = item.find('div',class_="front-search-date-share")
+			data['News Portal'].append("India Today")
+			data['Headlines'].append(headline.text)
+			data['URL'].append(str(link))
+			data['Date'].append(str(date.text))
+			dataframe_obj = pd.DataFrame(data,columns=('News Portal','Headlines','URL','Date'))
+	return dataframe_obj
 
-
-
-headlines_thebetter_india('haryana')
-
+writer = pd.ExcelWriter('test.xlsx',engine='xlsxwriter')
+df1 = headlines_timesnow('haryana')
+df2 = headlines_thebetter_india('haryana')
+df3 = headlines_indiatoday('haryana')
+df = pd.concat([df1, df2, df3],ignore_index=True)
+df.to_excel(writer)
+writer.save()
